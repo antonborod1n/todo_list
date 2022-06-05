19d31766e1a32c1c2f8f542ff7fe2ebcb4c3cd5e
antonborod1n <antonborodin86@gmail.com>
2022-06-05 17:31:40 +0300

    added homework 19

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//todo
 var inputData = document.querySelector('#inputTask');
 var btnAddTask = document.querySelector('#btn-add');
 var btnInfo = document.querySelector('#btn-info');
@@ -9,10 +10,14 @@ var infoModal = document.querySelector('.modals');
 var itemLi = document.getElementsByTagName('li');
 var activeSum = document.querySelector('.active');
 var noActiveSum = document.querySelector('.no-active');
+var newLi = document.createElement('li');
+
+//localStorage buttons
+var btnSave = document.querySelector('#btn-save');
+var btnClear = document.querySelector('#btn-clear');
 
 function createTask() {
     var inputValue = inputData.value;
-    
 
     if (inputData.value.trim() === '') {
         alert('Введите название');
@@ -35,6 +40,7 @@ function createTask() {
             counterToDo();
         });
     }
+
     removeTask();
     counterToDo();
 }
@@ -53,14 +59,52 @@ removeTask();
 
 function counterToDo() {
     var liSum = itemLi.length;
+
+    //console.log(liSum);
     var classSum = document.getElementsByClassName('line-through').length;
+
+    //console.log(classSum);
+    
     activeSum.innerHTML = liSum - classSum;
     noActiveSum.innerHTML = classSum;
-
 }
 
-counterToDo(); 
+counterToDo();
+
+//Модальное окно
 
 infoDev.addEventListener('click', function () {
     infoModal.classList.toggle('modals__block');
 });
+
+//localStorage
+
+btnSave.addEventListener('click', function () {
+    let activeCounter = document.querySelector('.active').innerHTML;
+    let noActiveCounter = document.querySelector('.no-active').innerHTML;
+    let ulList = document.querySelector('#list').innerHTML;
+
+    localStorage.setItem('active', activeCounter);
+    localStorage.setItem('no-active', noActiveCounter);
+    localStorage.setItem('item-li', ulList);
+});
+
+btnClear.addEventListener('click', function () {
+    localStorage.clear();
+});
+
+function addContent() {
+    activeSum.innerHTML = localStorage.getItem('active');
+    noActiveSum.innerHTML = localStorage.getItem('no-active');
+    ulList.innerHTML = localStorage.getItem('item-li');
+
+    for (let key of itemLi) {
+        key.onclick = function () {
+            key.classList.add('line-through');
+            counterToDo();
+        };
+    }
+    removeTask();
+}
+
+addContent();
